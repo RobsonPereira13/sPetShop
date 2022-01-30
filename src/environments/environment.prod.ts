9d588d8904352ce88c649359495cf6c06b98ba21
RobsonPereira13 <robsonpereirape@hotmail.com>
2022-01-30 16:47:25 -0300

    integração para deploy automatico git firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDCOAsy_FmOB9hxtayzYjDcnA0ETOPM8iU",
+    authDomain: "petshop-86992.firebaseapp.com",
+    projectId: "petshop-86992",
+    storageBucket: "petshop-86992.appspot.com",
+    messagingSenderId: "519978713468",
+    appId: "1:519978713468:web:3ad06663bf80bcc79fe4f9"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDCOAsy_FmOB9hxtayzYjDcnA0ETOPM8iU",
+    authDomain: "petshop-86992.firebaseapp.com",
+    projectId: "petshop-86992",
+    storageBucket: "petshop-86992.appspot.com",
+    messagingSenderId: "519978713468",
+    appId: "1:519978713468:web:3ad06663bf80bcc79fe4f9"
+  }
 };
 
 /*
